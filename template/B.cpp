97956ac6382e7af278dd591275fb2e0981699082
endeavorHao <1024403691@qq.com>
2023-07-15 11:39:24 +0800

    update

---
+++ template/B.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define x first
+#define y second
+#define int long long
+using namespace std;
+const int N = 200010, INF = 0x3f3f3f3f;
+typedef pair<int, int> PII;
+int n, m;
+int a[N];
+void sovle(){
+	
+}
+signed main() {
+	#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+	#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int T = 1;
+	cin >> T;
+	while(T -- ){
+		sovle();
+	}
+	return 0;
+}
+
+
