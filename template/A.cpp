539ba60115bd9e9e61afd7656e43bf1cec715c21
endeavorHao <1024403691@qq.com>
2023-07-16 22:05:53 +0800

    update

--- template/A.cpp
+++ template/A.cpp
@@ -5,10 +5,18 @@
 using namespace std;
 const int N = 200010, INF = 0x3f3f3f3f;
 typedef pair<int, int> PII;
-int n, m;
+int n, m, x;
 int a[N];
 void sovle(){
-	
+	cin >> x;
+    for(int i = 1; i <= 9; i ++ ){
+        int t = 0;
+        t = x * 10 + i;
+        if(t % 7 == 0){
+            cout << t << endl;
+            break;
+        }
+    }
 }
 signed main() {
 	#ifndef ONLINE_JUDGE
@@ -19,7 +27,7 @@ signed main() {
 	cin.tie(0);
 	cout.tie(0);
 	int T = 1;
-	cin >> T;
+	//cin >> T;
 	while(T -- ){
 		sovle();
 	}
--- template/template.cpp
+++ template/template.cpp
@@ -1,73 +1,29 @@
-// #include <bits/stdc++.h>
-// #define x first
-// #define y second
-// #define int long long
-// using namespace std;
-// const int N = 200010, INF = 0x3f3f3f3f;
-// typedef pair<int, int> PII;
-// int n, m;
-// int a[N];
-// void sovle(){
-	
-// }
-// signed main() {
-// 	ios::sync_with_stdio(false);
-// 	cin.tie(0);
-// 	cout.tie(0);
-// 	int T = 1;
-// 	cin >> T;
-// 	while(T -- ){
-// 		sovle();
-// 	}
-// 	return 0;
-// }
-
-#include<bitsdc++.h>
-using namespace std;
-
-#define int long long
+#include <bits/stdc++.h>
 #define x first
 #define y second
-#define endl "\n"
-int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
-
-typedef long long LL;
+#define int long long
+using namespace std;
+const int N = 200010, INF = 0x3f3f3f3f;
 typedef pair<int, int> PII;
-typedef pair<PII, int> PIII;
-typedef pair<PII, PII> PIIII;
-typedef pair<int, char> PIC;
-typedef pair<char, int> PCI;
-struct edge{
-  int a, b, c;
-  bool operator < (const edge &t) const{
-    return c < t.c;
-  }
-};
-
-int lowbit(int x) { return x & -x; }
-
-const int N = 2e5 + 10, M = 1e6 + 10, INF = 0x3f3f3f3f;
-int n, m, k, t;
-int arr[N];
-
-void solve(){
-
+int n, m;
+int a[N];
+void sovle(){
+	
+}
+signed main() {
+	#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+	freopen("out.txt", "w", stdout);
+	#endif
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int T = 1;
+	cin >> T;
+	while(T -- ){
+		sovle();
+	}
+	return 0;
 }
 
-signed main(){
-  #ifndef ONLINE_JUDGE
-  freopen("in.txt", "r", stdin);
-  freopen("out.txt", "w", stdout);
-  #endif
 
-  ios::sync_with_stdio(false);
-  cin.tie(0);
-
-  int T = 1;
-  cin >> T;
-  while(T--){
-    solve();
-  }
-
-  return 0;
-}
