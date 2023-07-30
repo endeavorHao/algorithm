c4513b61a8a1f9f579ccbac02d949b43757508d4
endeavorHao <1024403691@qq.com>
2023-07-30 22:52:43 +0800

    update

--- template/C.cpp
+++ template/C.cpp
@@ -3,31 +3,8 @@
 #define y second
 #define int long long
 using namespace std;
-typedef pair<int, int> PII;
 void solve(){
-	int n, m;
-	cin >> n >> m;
-	string s;
-	cin >> s;
-	set<PII> S;
-	vector<int> las(n + 1, -1), nex(n + 1, n);
-	for(int i = 0; i < n; i ++ ){
-		las[i + 1] = s[i] == '0' ? i : las[i];
-	}
-	for(int i = n - 1; i >= 0; i -- ){
-		nex[i] = s[i] == '1' ? i : nex[i + 1];
-	}
-	while(m -- ){
-		int l, r;
-		cin >> l >> r;
-		l --;
-		l = nex[l], r = las[r];
-		if(l > r){
-			l = r = -1;
-		}
-		S.emplace(l, r);
-	}
-	cout << S.size() << endl;
+	
 }
 signed main() {
 	#ifndef ONLINE_JUDGE
--- template/D.cpp
+++ template/D.cpp
@@ -4,34 +4,7 @@
 #define int long long
 using namespace std;
 void solve(){
-	int n;
-	cin >> n;
-	vector<int> a(n);
-	for(int i = 0; i < n; i ++ ) cin >> a[i];
-	vector<int> r(n);
-	iota(r.begin(), r.end(), 0);
-	vector<int> las(n + 1, 0), nex(n + 1, n - 1);
-	for(int i = 1; i < n - 1; i ++ ){
-		las[i] = a[i] == 0 ? i : las[i - 1];
-	}
-	for(int i = n - 1; i >= 0; i -- ){
-		nex[i] = a[i] == 0 ? i : nex[i + 1];
-	}
-	for(int i = 0; i < n; i ++ ){
-		if(a[i] == 1){
-			r[las[i]] = max(r[las[i]], i);
-			r[i] = max(r[i], nex[i]);
-		}else if(a[i] == 2){
-			r[las[i]] = max(r[las[i]], nex[i]);
-		}
-	}
-	for(int i = 1; i < n; i ++ )
-		r[i] = max(r[i], r[i - 1]);
-	int ans = 0;
-	for(int i = 0; i < n; i = r[i] + 1){
-		ans ++;
-	}
-	cout << ans << endl;
+	
 }
 signed main() {
 	#ifndef ONLINE_JUDGE
@@ -42,7 +15,7 @@ signed main() {
 	cin.tie(0);
 	cout.tie(0);
 	int T = 1;
-	// cin >> T;
+	cin >> T;
 	while(T -- ){
 		solve();
 	}
