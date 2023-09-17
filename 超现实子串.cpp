c29df07e75759cabc7b3ae34990c2a6d56795485
endeavorHao <1024403691@qq.com>
2023-09-17 17:31:27 +0800

    update

---
+++ 超现实子串.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define x first
+#define y second
+#define int long long
+#define endl "\n"
+#define all(v) v.begin(), v.end()
+using namespace std;
+const int N = 200010, INF = 0x3f3f3f3f, mod = 1000000007;
+typedef pair<int, int> PII;
+template <typename _T>
+inline void read(_T &f) {
+    f = 0; _T fu = 1; char c = getchar();
+    while (c < '0' || c > '9') { if (c == '-') { fu = -1; } c = getchar(); }
+    while (c >= '0' && c <= '9') { f = (f << 3) + (f << 1) + (c & 15); c = getchar(); }
+    f *= fu;
+}
+ 
+template <typename T>
+void print(T x) {
+    if (x < 0) putchar('-'), x = -x;
+    if (x < 10) putchar(x + 48);
+    else print(x / 10), putchar(x % 10 + 48);
+}
+ 
+template <typename T>
+void print(T x, char t) {
+    print(x); putchar(t);
+}
+void solve(){
+	int n;
+    read(n);
+    vector<int> a(n + 1);
+    for(int i = 1; i <= n; i ++ ) read(a[i]);
+    int ans = 0;
+    for(int i = 1; i <= n; i ++ ){
+        int j = i;
+        while(j < n && a[j + 1] == a[i] + ((j - i) % 2 ? -1: 1) * ((j - i + 2) / 2)) j ++;
+        ans = max(ans, j - i + 1);
+    }
+    print(ans);
+}
+signed main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int T = 1;
+	// cin >> T;
+	while(T -- ){
+		solve();
+	}
+	return 0;
+}
